7eaa8eb7f8da46ed1eb2dfe989972d2361ddf09f
Elizabeth1047 <egharevbalizzy5@gmail.com>
2023-03-11 13:51:15 -0800

    image  done

--- pages/components/Headercards.tsx
+++ pages/components/Headercards.tsx
@@ -6,21 +6,21 @@ interface props {
   text: string;
 }
 
-export default function Headercards({imgs, title, text}:props) {
+export default function Headercards({ imgs, title, text }: props) {
   return (
-      <section
-            className=" border-x-2 border-y-2 border-gray-200 
+    <section
+      className=" border-x-2 border-y-2 border-gray-200 
             flex justify-between items-center sm:w-[59%] 
           md:max-w-[30%] lg:max-w-[23%]  xl:max-w-[26%] p-3   rounded-lg mx-3"
-      >
-        <div className="flex  gap-4  items-center">
-          <div className="">{imgs}</div>
-          <div>
-            <h6 className="font-semibold">{title}</h6>
-            <p className="hidden sm:block text-sm text-gray-400">{text}</p>
-          </div>
+    >
+      <div className="flex  gap-4  items-center">
+        <div className="">{imgs}</div>
+        <div>
+          <h6 className="font-semibold">{title}</h6>
+          <p className="hidden sm:block text-sm text-gray-400">{text}</p>
         </div>
-        <AiOutlinePlus size={20} />
-      </section>
+      </div>
+      <AiOutlinePlus size={20} />
+    </section>
   );
 }
--- pages/components/Subheadercards.tsx
+++ pages/components/Subheadercards.tsx
@@ -1,16 +1,17 @@
 import { FcPicture } from "react-icons/fc";
+import Image from "next/image";
 
-interface props {
-  imglink: string;
-}
 
-export default function Subheadercards({imglink:any}:props) {
+export default function Subheadercards() {
   return (
     <section className="w-[80%]">
-      {/* <FcPicture size={200} /> */}
       <div className="w-[100%] my-5 ">
-        <img
-          src="https://static.figma.com/uploads/cb7a96dc6ea6f0a0b8923c232a08611b8336a67a"
+        <Image
+          src="/image/dec5a579e129f850d9cb185e10eb9cb857c549b3.png"
+          width={200}
+          height={200}
+          alt="my"
+          className="rounded-xl"
         />
       </div>
       <p className="text-base ml-0">Diagram basic in Figjam</p>
